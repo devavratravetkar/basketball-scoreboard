079c2e0562b83a9030a9ca2870f8ed519f569034
Devavrat Ravetkar <devavrat.ravetkar@yahoo.com>
2025-11-08 12:37:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function homePlusOne(){
+    document.getElementById("home-score").textContent++
+}
+
+function homePlusTwo(){
+    homePlusOne()
+    homePlusOne()
+}
+
+function homePlusThree(){
+    homePlusOne()
+    homePlusOne()
+    homePlusOne()
+}
+
+function guestPlusOne(){
+    document.getElementById("guest-score").textContent++
+}
+
+function guestPlusTwo(){
+    guestPlusOne()
+    guestPlusOne()
+}
+
+function guestPlusThree(){
+    guestPlusOne()
+    guestPlusOne()
+    guestPlusOne()
+}
